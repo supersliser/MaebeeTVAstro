e1a356c39555a0a40ffb2890e08f92415657e907
supersliser <thomaslower9@gmail.com>
2023-05-08 11:53:55 +0100

    egsdfsdfs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/static";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  adapter: vercel()
 });
