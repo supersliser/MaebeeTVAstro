95d51d52e6d7fca2f78f8a534f55cd2aedd8a9bb
supersliser <thomaslower9@gmail.com>
2023-05-11 20:20:04 +0100

    dfijnjfi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
  
 export default defineConfig({
-  output: 'static',
-  adapter: vercelStatic(),
+  output: 'server',
+  adapter: vercel(),
 });
