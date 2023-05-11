8ab4144923bfe3dfc27eb50818723ed5c2ddc70b
supersliser <thomaslower9@gmail.com>
2023-05-11 19:58:14 +0100

    dfsg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/static";
-import vercelStatic from '@astrojs/vercel/static';
-
-
-// https://astro.build/config
+import vercelEdge from '@astrojs/vercel/edge';
+ 
 export default defineConfig({
-  output: "server",
-  adapter: vercelStatic()
+  output: 'server',
+  adapter: vercelEdge(),
 });
