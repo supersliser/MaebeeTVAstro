9ad7e5936bdb94a192378c254f02203f34123e8f
supersliser <thomaslower9@gmail.com>
2023-05-23 18:02:21 +0100

    poii

---
+++ .vercel/output/static/chunks/pages/index.astro.c4053d29.mjs
@@ -0,0 +1,93 @@
+import { c as createAstro, a as createComponent, r as renderTemplate, f as renderComponent, m as maybeRenderHead } from '../astro.bfb7bb32.mjs';
+import 'html-escaper';
+import { $ as $$BasicLayout, a as $$BasicKidsLayout } from './_...glob_.astro.d002ebdc.mjs';
+/* empty css                               *//* empty css                           *//* empty css                           *//* empty css                           *//* empty css                           */
+const $$Astro$1 = createAstro();
+const $$Index$1 = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$Index$1;
+  const title = "Home";
+  const subtitle = "Changing the world, one story at a time";
+  const PrimaryColor = "#ff9dd0";
+  const NavTextColor = "#000000";
+  return renderTemplate`${renderComponent($$result, "BasicLayout", $$BasicLayout, { "title": title, "subtitle": subtitle, "PrimaryColor": PrimaryColor, "NavTextColor": NavTextColor, "class": "astro-J7PV25F6" }, { "default": ($$result2) => renderTemplate`
+	${maybeRenderHead($$result2)}<div class="vertical main marginTop astro-J7PV25F6">
+		<div class="horizontal astro-J7PV25F6">
+			<div class="double astro-J7PV25F6">
+				<img src="https://media.discordapp.net/attachments/903265431396179978/977713498090008606/MaebeeTV-logos.jpeg" alt="The MaebeeTV logo." width="400px" height="400px" class="astro-J7PV25F6">
+			</div>
+			<div class="vertical double astro-J7PV25F6">
+				<h1 class="serif astro-J7PV25F6">Welcome to MaebeeTV!</h1>
+				<p class="serif astro-J7PV25F6">
+					We are a movie production company that strives to change the
+					way stories are told.</p><p class="astro-J7PV25F6"></p>
+				
+			</div>
+		</div>
+		<div class="vertical marginTop2 astro-J7PV25F6">
+			<h1 class="serif astro-J7PV25F6">Want to join our team?</h1>
+			<p class="serif astro-J7PV25F6">
+				Our company is commited to providing safe and friendly
+				environments that can ensure the highest quality productions are
+				created. Our films are made as a single team, with smaller
+				groups assigned based on your own unique talents and quirks.
+				Each team is given important tasks with flexible deadlines, all
+				in the spirit of raising awareness and encouraging others to
+				share their stories.
+			</p>
+			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdaBYNVmnUodH3BqEIymy7LIrWOhiGqPkH2OF0i-wPWj-YkQA/viewform?embedded=true" class="astro-J7PV25F6">
+				Loading Google Form...
+			</iframe>
+		</div>
+	</div>
+` })}`;
+}, "M:/maebeetv stuff/MaebeeTVAstro/src/pages/index.astro");
+
+const $$file$1 = "M:/maebeetv stuff/MaebeeTVAstro/src/pages/index.astro";
+const $$url$1 = "";
+
+const index$1 = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	default: $$Index$1,
+	file: $$file$1,
+	url: $$url$1
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const $$Astro = createAstro();
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  const title = "Home";
+  const subtitle = "Changing the world, one story at a time";
+  const PrimaryColor = "#fffb0d";
+  const NavTextColor = "#000000";
+  return renderTemplate`${renderComponent($$result, "BasicLayout", $$BasicKidsLayout, { "title": title, "subtitle": subtitle, "PrimaryColor": PrimaryColor, "NavTextColor": NavTextColor, "class": "astro-7ORWTOUU" }, { "default": ($$result2) => renderTemplate`
+	${maybeRenderHead($$result2)}<div class="vertical main marginTop astro-7ORWTOUU">
+		<div class="horizontal astro-7ORWTOUU">
+			<div class="double astro-7ORWTOUU">
+				<img src="https://cdn.discordapp.com/attachments/903258487872700520/1110100037825990746/MaebeeTVKids.png" alt="The MaebeeTV logo." width="400px" height="400px" class="astro-7ORWTOUU">
+			</div>
+			<div class="vertical double astro-7ORWTOUU">
+				<h1 class="sans astro-7ORWTOUU">Welcome to MaebeeTVKids!!!</h1>
+				<p class="sans astro-7ORWTOUU">
+					We're a magical movie-making crew that loves to make stories come to life in the most amazing and fun ways ever!
+				</p>
+			</div>
+		</div>
+		<div class="marginTop astro-7ORWTOUU"></div>
+		<div class="marginTop astro-7ORWTOUU"></div>
+	</div>
+` })}`;
+}, "M:/maebeetv stuff/MaebeeTVAstro/src/pages/other/kids/index.astro");
+
+const $$file = "M:/maebeetv stuff/MaebeeTVAstro/src/pages/other/kids/index.astro";
+const $$url = "/other/kids";
+
+const index = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	default: $$Index,
+	file: $$file,
+	url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { index as a, index$1 as i };
---
+++ .vercel/output/static/chunks/pages/privacypolicy.astro.73f6bcbc.mjs
@@ -0,0 +1,77 @@
+import { c as createAstro, a as createComponent, r as renderTemplate, f as renderComponent, m as maybeRenderHead } from '../astro.bfb7bb32.mjs';
+import 'html-escaper';
+import { $ as $$BasicLayout } from './_...glob_.astro.d002ebdc.mjs';
+/* empty css                                   *//* empty css                               *//* empty css                               *//* empty css                           *//* empty css                           *//* empty css                               *//* empty css                               *//* empty css                               */
+const $$Astro = createAstro();
+const $$Privacypolicy = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Privacypolicy;
+  const title = "Privacy Policy";
+  const subtitle = "Our Privacy Policy";
+  const PrimaryColor = "#ff9dd0";
+  const NavTextColor = "#000000";
+  return renderTemplate`${renderComponent($$result, "BasicLayout", $$BasicLayout, { "title": title, "subtitle": subtitle, "PrimaryColor": PrimaryColor, "NavTextColor": NavTextColor, "class": "astro-AJFUGSEO" }, { "default": ($$result2) => renderTemplate`
+    ${maybeRenderHead($$result2)}<div class="astro-AJFUGSEO">
+        <p class="serif astro-AJFUGSEO">
+            At Maebeetv.com, we are committed to protecting the privacy and
+            security of our users. This Privacy Policy explains how we collect,
+            use, and disclose personal information that we may receive when you
+            use our website.
+        </p><p class="serif astro-AJFUGSEO">Cookies and Personalized Advertising</p><p class="serif astro-AJFUGSEO">
+            Like many other websites, we use cookies to enhance your user
+            experience and serve personalized advertising. Third-party vendors,
+            including Google, use cookies to serve ads based on a user's prior
+            visits to your website or other websites.
+        </p><p class="serif astro-AJFUGSEO">
+            Google's use of advertising cookies enables it and its partners to
+            serve ads to your users based on their visit to your sites and/or
+            other sites on the Internet. Users may opt-out of personalized
+            advertising by visiting Ads Settings. (Alternatively, you can direct
+            users to opt-out of a third-party vendor's use of cookies for
+            personalized advertising by visiting www.aboutads.info.)
+        </p><p class="serif astro-AJFUGSEO">
+            If you have not opted out of third-party ad serving, the cookies of
+            other third-party vendors or ad networks may also be used to serve
+            ads on your site. In this case, we notify our site visitors of the
+            third-party vendors and ad networks serving ads on our site, and
+            provide links to the appropriate vendor and ad network websites. We
+            also inform our users that they may visit those websites to opt-out
+            of the use of cookies for personalized advertising (if the vendor or
+            ad network offers this capability). Information Collection and Use.
+        </p><p class="serif astro-AJFUGSEO">
+            When you visit Maebeetv.com, we automatically receive and record
+            information on our server logs from your browser, including your IP
+            address, cookie information, and the pages you visit on our site. We
+            may use this information to analyze trends, administer the site,
+            track user movements around the site, and gather demographic
+            information about our user base as a whole.
+        </p><p class="serif astro-AJFUGSEO">
+            We may also collect personal information that you voluntarily
+            provide to us, such as your name and email address when you
+            subscribe to our newsletter or contact us through our website. We
+            will never sell or rent your personal information to any third party
+            without your consent.
+        </p><p class="serif astro-AJFUGSEO">Links to Other Sites</p><p class="serif astro-AJFUGSEO">
+            Maebeetv.com may contain links to other websites that are not owned
+            or operated by us. We are not responsible for the privacy practices
+            or content of these websites. We encourage you to review the privacy
+            policies of these websites before providing any personal
+            information.
+        </p><p class="serif astro-AJFUGSEO">Changes to this Privacy Policy</p><p class="serif astro-AJFUGSEO">
+            We may update this Privacy Policy from time to time, and we will
+            post the updated version on our website. We encourage you to review
+            this Privacy Policy periodically to stay informed about our
+            information practices and the ways you can help protect your
+            privacy.
+        </p><p class="serif astro-AJFUGSEO">Contact Us</p><p class="serif astro-AJFUGSEO">
+            If you have any questions or concerns about this Privacy Policy,
+            please contact us at contactmaebeeproductions@gmail.com.
+        </p>
+    </div>
+` })}`;
+}, "M:/maebeetv stuff/MaebeeTVAstro/src/pages/other/privacypolicy.astro");
+
+const $$file = "M:/maebeetv stuff/MaebeeTVAstro/src/pages/other/privacypolicy.astro";
+const $$url = "/other/privacypolicy";
+
+export { $$Privacypolicy as default, $$file as file, $$url as url };
